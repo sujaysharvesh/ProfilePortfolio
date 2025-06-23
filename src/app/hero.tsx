a57a3410062fce22455f1a1f50854a0c7f281981
sujaySharvesh <sujaysharvesh098@gmail.com>
2025-06-23 22:25:54 +0530

    fix small issue

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
+
 
 import { Typography } from "@material-tailwind/react";
 import { motion } from "framer-motion";
--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
 
 import { EyeIcon, CodeBracketIcon } from "@heroicons/react/24/outline";
